1cda9c62418940c09e38e7a93e4e1e9150fd68f9
Andrew Nguyen <andrewdnguyen64@gmail.com>
2018-03-01 10:51:06 -0800

    third party API lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+	var latlng = {lat:32.8788033, lng:-117.2381008};
+
+	var map = new google.maps.Map(document.getElementById('map'),
+    {
+        center:latlng,
+            zoom:15
+    });
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map:map,
+        title:"UCSD"
+    });
 }
